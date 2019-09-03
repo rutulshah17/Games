af2d9b0a169da10857f3ba75f112078e59e7bdb6
Rutul Shah <rutulshah17@gmail.com>
2019-09-03 12:42:25 -0400

    adding bootstrap

--- config/globals.js
+++ config/globals.js
@@ -1,4 +1,4 @@
 module.exports = {
   //db: 'mongodb://localhost/js' // for local mongodb connection where js is the database name
-  'db': 'mongodb://*****:******@ds147207.mlab.com:47207/js'
+    'db': 'mongodb://admin:admin1@ds147207.mlab.com:47207/js'
 }
